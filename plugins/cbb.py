a35bf141389a43dc9e6ac3e1bba4ad4d69eed65d
kaustubh090 <126164591+kaustubh090@users.noreply.github.com>
2023-08-17 14:09:48 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>Premium Benefits & Perks</b>\nDirect Channel Links, No Ad Links\nSpecialAccess In Events\n<b>Pricing Rates</b>\n\n <pre><code>1</code></pre> Day - <pre><code>INR 20</code></pre>\n 7 Days - INR 40\n 1 Month - INR 100\n 3 Months - INR 200\n 6 Months - INR 400\n 9 Months - INR 600\n 12 Months - INR 800\n <b>Want To Buy?</b>\n Pay Using UPI killuaog@upi\n Send Screenshot to @killua_og and in this bot (To Process Auto Verification)\n <b>We Have Limited Seats For Premium Users</b>",
+            text = f"<b>Premium Benefits & Perks</b>\nDirect Channel Links, No Ad Links\nSpecialAccess In Events\n\n<b>Pricing Rates</b>\n <pre><code>1</code></pre> Day - <pre><code>INR 20</code></pre>\n 7 Days - INR 40\n 1 Month - INR 100\n 3 Months - INR 200\n 6 Months - INR 400\n 9 Months - INR 600\n 12 Months - INR 800\n <b>Want To Buy?</b>\n Pay Using UPI killuaog@upi\n Send Screenshot to @killua_og and in this bot (To Process Auto Verification)\n <b>We Have Limited Seats For Premium Users</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
