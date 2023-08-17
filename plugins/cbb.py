e7004a0e5e88a0d677d121ebd4e43147698bc988
kaustubh090 <126164591+kaustubh090@users.noreply.github.com>
2023-08-17 14:27:41 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>Premium Benefits & Perks</b>\nDirect Channel Links, No Ad Links\nSpecialAccess In Events\n\n<b>Pricing Rates</b>\n<code>1</code> Day - <code>INR 20</code>\n 7 Days - INR 40\n 1 Month - INR 100\n 3 Months - INR 200\n 6 Months - INR 400\n 9 Months - INR 600\n 12 Months - INR 800\n <b>Want To Buy?</b>\n Pay Using UPI killuaog@upi\n Send Screenshot to @killua_og and in this bot (To Process Auto Verification)\n <b>We Have Limited Seats For Premium Users</b>",
+            text = f"<b>Premium Benefits & Perks</b>\n<i>Direct Channel Links, No Ad Links</i>\n<i>SpecialAccess In Events</i>\n\n<b>Pricing Rates</b>\n<code>1</code> Day - <code>INR 20</code>\n<code>7</code> Days - <code>INR 40</code>\n<code>1</code> Month - <code>INR 100</code>\n<code>3</code> Months - <code>INR 200</code>\n<code>6</code> Months - <code>INR 400</code>\n<code>9</code> Months - <code>INR 600</code>\n<code>12</code> Months - <code>INR 800</code>\n\n<b>Want To Buy?💓</b>\n Pay Using UPI <code>killuaog@upi</code>\n Send Screenshot to @killua_og and in this bot <i>(To Process Auto Verification)</i>\n\n○<b>We Have Limited Seats For Premium Users</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
