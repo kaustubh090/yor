b1993996b64a30170e47244a6139c2245ebabf7c
kaustubh090 <126164591+kaustubh090@users.noreply.github.com>
2023-11-03 01:19:43 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>Premium Benefits & Perks</b>\n<i>Direct Channel Links, No Ad Links</i>\n<i>SpecialAccess In Events</i>\n\n<b>Pricing Rates</b>\n<code>1</code> Day - <code>INR 20</code>\n<code>7</code> Days - <code>INR 40</code>\n<code>1</code> Month - <code>INR 100</code>\n<code>3</code> Months - <code>INR 200</code>\n<code>6</code> Months - <code>INR 400</code>\n<code>9</code> Months - <code>INR 600</code>\n<code>12</code> Months - <code>INR 800</code>\n\n<b>Want To Buy?💓</b>\n<b>»</b> Pay Using UPI <code>killuaog@upi</code>\n<b>»</b> Send Screenshot to @killua_og and in this bot <i>(To Process Auto Verification)</i>\n\n○ <b>We Have Limited Seats For Premium Users</b>",
+            text = f"<b>Premium Benefits & Perks</b>\n<i>Direct Channel Links, No Ad Links</i>\n<i>SpecialAccess In Events</i>\n\n<b>Pricing Rates</b>\n<code>1</code> Day - <code>INR 20</code>\n<code>7</code> Days - <code>INR 40</code>\n<code>1</code> Month - <code>INR 100</code>\n<code>3</code> Months - <code>INR 200</code>\n<code>6</code> Months - <code>INR 400</code>\n<code>9</code> Months - <code>INR 600</code>\n<code>12</code> Months - <code>INR 800</code>\n\n<b>Want To Buy?💓</b>\n<b>»</b> Pay Using UPI <code>vilaskachole68414-1@okicici</code>\n<b>»</b> Send Screenshot to @killua_og and in this bot <i>(To Process Auto Verification)</i>\n\n○ <b>We Have Limited Seats For Premium Users</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
